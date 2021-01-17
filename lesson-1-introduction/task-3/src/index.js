8bc53f3b581cd66ff46cbf03d360bb34d1d5e55e
Ihor <ihor.sudom@gmail.com>
2021-01-17 10:30:28 +0200

    option

--- lesson-1-introduction/task-3/src/index.js
+++ lesson-1-introduction/task-3/src/index.js
@@ -5,9 +5,9 @@ import './styles.css';
 const rootElement = document.querySelector('#root');
 
 const element = (
-  <div class="greeting">
-    <div class="greeting__title">Hello, world</div>
-    <div class="greeting__text">I'm learning React</div>
+  <div className="greeting">
+    <div className="greeting__title">Hello, world</div>
+    <div className="greeting__text">I'm learning React</div>
   </div>
 );
 
