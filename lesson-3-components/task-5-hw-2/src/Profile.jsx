f02adaa23cca097b46d267725ebb5506bba53345
Ihor <ihor.sudom@gmail.com>
2021-01-18 19:52:03 +0200

    option

--- lesson-3-components/task-5-hw-2/src/Profile.jsx
+++ lesson-3-components/task-5-hw-2/src/Profile.jsx
@@ -3,11 +3,11 @@ import moment from "moment";
 
 
 const Profile = (props) => {
-  const age = moment(props.userData.birthDate).format("D MMM YY");
+  const age = moment(props.userData.birthDate).format("DD MMM YY");
   return (
-    <div>
-      <div className="profile__name">{props.userData.firstName} {props.userData.lastName}</div>
-      <div className="profile__birth">Was born {age} in {props.userData.birthPlace}</div>
+    <div className="profile">
+      <p className="profile__name">{props.userData.firstName} {props.userData.lastName}</p>
+      <p className="profile__birth">Was born {age} in {props.userData.birthPlace}</p>
     </div>
   );
 };
