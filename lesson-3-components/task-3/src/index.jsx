c350159fadda5e076ef245d5eb5d9a1c9750bf1a
Ihor <ihor.sudom@gmail.com>
2021-01-18 16:15:47 +0200

    option

--- lesson-3-components/task-3/src/index.jsx
+++ lesson-3-components/task-3/src/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Comment from './Comment.jsx';
+import './styles.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Comment />, rootElement);
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com',
+};
+
+
+ReactDOM.render(
+  <Comment 
+    user={userInfo}
+    text="Good job!"
+    date={new Date()}
+  />,
+  rootElement
+);
