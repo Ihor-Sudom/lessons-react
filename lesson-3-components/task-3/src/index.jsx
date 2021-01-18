94a1f48a9d5ea4d2d585c64bbdb1c6e426e1ccf8
Ihor <ihor.sudom@gmail.com>
2021-01-18 15:56:57 +0200

    option

---
+++ lesson-3-components/task-3/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Comment from './Comment.jsx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Comment />, rootElement);
