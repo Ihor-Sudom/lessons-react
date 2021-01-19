3b6029da9d0319499b797a2531d2cd02c32a00fb
Ihor <ihor.sudom@gmail.com>
2021-01-19 14:51:01 +0200

    option

---
+++ lesson-5-event/task-1/src/GoodButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GoodButton = () => (
+    <button 
+    className="fancy-button"
+    onClick={() => alert('Good jod!')}>
+    Click me!
+  </button>
+);
+
+export default GoodButton;
