863736915c7568655258ccea7b82036ba0c55098
Ihor <ihor.sudom@gmail.com>
2021-01-19 16:08:14 +0200

    option

--- lesson-5-event/task-3/src/Counter.jsx
+++ lesson-5-event/task-3/src/Counter.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Counter extends React.Component {
+class Counter extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -13,36 +13,36 @@ class Counter extends React.Component {
   decrement() {
     this.setState({
       counter: this.state.counter - 1,
-    })
+    });
   }
 
   increment = () => {
     this.setState({
       counter: this.state.counter + 1,
-    })
+    });
   }
 
   reset() {
     this.setState({
       counter: 0,
-    })
+    });
   }
 
   render() {
     return (
-      <div class="counter">
-        <button data-action="decrease" class="counter__button" onClick={this.decrement}>
+      <div className="counter">
+        <button data-action="decrease" className="counter__button" onClick={this.decrement}>
           -
         </button>
-        <span class="counter__value" onClick={() => this.reset()}>
+        <span className="counter__value" onClick={() => this.reset()}>
           {this.state.counter}
         </span>
-        <button data-action="increase" class="counter__button" onClick={this.increment}>
+        <button data-action="increase" className="counter__button" onClick={this.increment}>
           +
         </button>
       </div>
-    )
-  }
+    );
+  };
 }
 
 export default Counter;
