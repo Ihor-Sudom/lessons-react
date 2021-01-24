33df2d4df125e89a4937491034ef778b7a426ff1
Ihor <ihor.sudom@gmail.com>
2021-01-24 19:33:16 +0200

    option

--- lesson-7-list/task-4-dop/src/UsersList.jsx
+++ lesson-7-list/task-4-dop/src/UsersList.jsx
@@ -6,7 +6,7 @@ class UsersList extends React.Component {
   constructor(props) {
     super(props),
     this.state = {
-      limit: 3,
+      limitElement: 3,
       page: 1,
       startIndex: 0,
       endIndex: 0,
@@ -16,18 +16,18 @@ class UsersList extends React.Component {
   handleGoNext = () => {
     this.setState({
       page: this.state.page + 1,
-      startIndex: this.state.startIndex + this.state.limit,
+      startIndex: this.state.startIndex + this.state.limitElement,
       endIndex: this.state.endIndex 
-        ? this.state.endIndex + this.state.limit
-        : this.state.limit * 2,
+        ? this.state.endIndex + this.state.limitElement
+        : this.state.limitElement * 2,
     })
   }
 
   handleGoPrevios = () => {
     this.setState({
       page: this.state.page - 1,
-      startIndex: this.state.startIndex - this.state.limit,
-      endIndex: this.state.endIndex - this.state.limit,
+      startIndex: this.state.startIndex - this.state.limitElement,
+      endIndex: this.state.endIndex - this.state.limitElement,
     })
   }
 
@@ -40,10 +40,10 @@ class UsersList extends React.Component {
               goNext={this.handleGoNext}
               currentPage={this.state.page}
               totalItems={this.props.users.length}
-              itemsPerPage={this.state.limit} />
+              itemsPerPage={this.state.limitElement} />
             <ul className="users">
               {this.props.users
-                .slice(this.state.startIndex, this.state.endIndex || this.state.limit)
+                .slice(this.state.startIndex, this.state.endIndex || this.state.limitElement)
                 .map(user => (<User key={user.id} {...user} />))
               }
             </ul>
