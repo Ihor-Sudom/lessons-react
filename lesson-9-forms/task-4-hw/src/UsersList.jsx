2d6138fc10eb22b561726b0f8623cf83ef7312f9
Ihor <ihor.sudom@gmail.com>
2021-01-25 14:16:24 +0200

    option

--- lesson-9-forms/task-4-hw/src/UsersList.jsx
+++ lesson-9-forms/task-4-hw/src/UsersList.jsx
@@ -27,7 +27,7 @@ class UsersList extends Component {
           onChange={this.handleChange}
         />
         <ul className="users">
-            {userList.map(user => (<User key={user.id} name={user.name} age={user.age} />))}
+            {userList.map(user => (<User key={user.id} {...user} />))}
         </ul>
       </div>
     )
