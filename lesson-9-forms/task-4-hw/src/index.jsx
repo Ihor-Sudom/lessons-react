b66b3044cf01446a040713f376376dec0781898c
Ihor <ihor.sudom@gmail.com>
2021-01-25 13:15:57 +0200

    option

--- lesson-9-forms/task-4-hw/src/index.jsx
+++ lesson-9-forms/task-4-hw/src/index.jsx
@@ -5,32 +5,5 @@ import UsersList from './UsersList.jsx';
 
 const rootElement = document.querySelector('#root');
 
-const users = [
-  {
-      id: 'id-0',
-      age: 21,
-      name: 'Bob',
-  },
-  {
-      id: 'id-1',
-      age: 17,
-      name: 'Tom',
-  },
-  {
-      id: 'id-2',
-      age: 18,
-      name: 'Tad',
-  },
-  {
-      id: 'id-3',
-      age: 45,
-      name: 'Sam',
-  },
-  {
-    id: 'id-4',
-    age: 17,
-    name: 'Tom',
-},
-];
 
 ReactDOM.render(<UsersList users={users} />, rootElement);
