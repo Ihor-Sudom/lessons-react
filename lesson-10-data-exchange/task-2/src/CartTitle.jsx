c44e3ef9c37667cb73a3d2244c3d7f669a233291
Ihor <ihor.sudom@gmail.com>
2021-01-26 11:33:51 +0200

    option

--- lesson-10-data-exchange/task-2/src/CartTitle.jsx
+++ lesson-10-data-exchange/task-2/src/CartTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CartTitle = ({ userName, count }) => {
   return (
     <div className="cart-title">
-      {`${userName}, you added ${count} items`}
+      {userName}, you added {count} items
     </div>
   );
 };
