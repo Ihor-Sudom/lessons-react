091749e2fc03aab27de9a5adaf9e289e6dea063a
Ihor <ihor.sudom@gmail.com>
2021-01-26 14:16:03 +0200

    option

--- lesson-10-data-exchange/task-3-hw/src/index.jsx
+++ lesson-10-data-exchange/task-3-hw/src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.scss';
-import Search from './Search.jsx';
+import Calculator from './Calculator';
 
 const rootElement = document.querySelector('#root');
 
 
-ReactDOM.render(<Search />, rootElement);
+ReactDOM.render(<Calculator />, rootElement);
