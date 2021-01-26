6de94938d6865026b8f2b503f70ff89232ab1dfb
Ihor <ihor.sudom@gmail.com>
2021-01-26 20:11:02 +0200

    option

---
+++ lesson-11-update-components/task-2/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import Search from './Search.jsx';
+
+const rootElement = document.querySelector('#root');
+
+
+ReactDOM.render(<Search />, rootElement);
